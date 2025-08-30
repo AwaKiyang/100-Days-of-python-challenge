11d7ed6c50449d56a2eb9b9ff23f5ce090ad631b
AwaKiyang <awakiyang9@gmail.com>
2025-08-30 11:55:41 +0100

    commit day 20

--- day20/main.py
+++ day20/main.py
@@ -1,46 +1,22 @@
 from turtle import Turtle,Screen
+from snake_game import Snake
 import time
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor('black')
 screen.title("My snake game")
 screen.tracer(0)
-snake_list = list()
-spacing = 0
-  
-for snake in range(3):
-    snake_body = Turtle(shape="square")
-    snake_body.color('white')
-    snake_body.penup()
-    snake_body.goto(x = 0 + spacing, y=0)
-    spacing-=20
-    snake_list.append(snake_body)
+snake = Snake()
 
 
+snake.create_snake()
 proceed = True
 while proceed == True:
     screen.update()
-    time.sleep(0.1)
-
-    for i in range(len(snake_list) -1,0,-1):
-        tp = snake_list[i-1].pos()
-        snake_list[i].setpos(tp) 
+    time.sleep(0.15)
+    snake.move_snake()
+    snake.control_snake()    
 
 
-    snake_list[0].forward(20)
-    def right():
-        snake_list[0].setheading(0)
-    def up():
-        snake_list[0].setheading(90)
-    def left():
-        snake_list[0].setheading(180)
-    def down():
-        snake_list[0].setheading(270)
-
-    screen.onkey(right, "Right")
-    screen.onkey(up, "Up")
-    screen.onkey(left, "Left")
-    screen.onkey(down, "Down")
-    screen.listen() 
-        
 screen.exitonclick()
+
--- day20/snake_game.py
+++ day20/snake_game.py
@@ -1,33 +1,43 @@
-    from turtle import Turtle,Screen
-    screen = Screen()
-    spacing = 0
-    snake_list = list()
+from turtle import Turtle,Screen
+screen = Screen()
+class Snake:
 
+    def __init__(self):
+        self.snake_list = list()
+        self.spacing = 0
 
+    def create_snake(self):
+        for snake in range(3):
+            snake_body = Turtle(shape="square")
+            snake_body.color('white')
+            snake_body.penup()
+            snake_body.goto(x = 0 + self.spacing, y=0)
+            self.spacing-=20
+            self.snake_list.append(snake_body)
 
- # Move the snake segments from tail to head
-    for i in range(len(snake_list) - 1, 0, -1):
-        new_x = snake_list[i - 1].xcor()
-        new_y = snake_list[i - 1].ycor()
-        snake_list[i].setpos(new_x, new_y)
+    def move_snake(self):
+        for i in range(len(self.snake_list) -1,0,-1):
+            tp = self.snake_list[i-1].pos()
+            self.snake_list[i].setpos(tp) 
 
-    snake_list[0].forward(20)
+        self.snake_list[0].forward(20)
 
-    def right():
-        snake_list[0].setheading(0)
-    def up():
-        snake_list[0].setheading(90)
-    def left():
-        snake_list[0].setheading(180)
-    def down():
-        snake_list[0].setheading(270)
+    def control_snake(self):
+        def right():
+            if self.snake_list[0].heading() != 180:
+                self.snake_list[0].setheading(0)  
+        def up():
+            if self.snake_list[0].heading() != 270:
+                self.snake_list[0].setheading(90)
+        def left(): 
+            if self.snake_list[0].heading() != 0:
+                self.snake_list[0].setheading(180)
+        def down():
+            if self.snake_list[0].heading() != 90:
+                self.snake_list[0].setheading(270)
 
-    screen.onkey(right, "Right")
-    screen.onkey(up, "Up")
-    screen.onkey(left, "Left")
-    screen.onkey(down, "Down")
-    screen.listen()
-        
-
-
-    screen.exitonclick()
+        screen.onkey(right, "Right")
+        screen.onkey(up, "Up")
+        screen.onkey(left, "Left")
+        screen.onkey(down, "Down")
+        screen.listen() 
